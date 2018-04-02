f8c978b17d7e368e7ef57f3c1f5aa3f303cd668a
kodera2t <kodera2t@yahoo.co.jp>
2018-04-02 13:38:10 +0900

    Files for simple Alexa board

---
+++ components/alexa/alexa_speech_recognizer.c
@@ -0,0 +1,189 @@
+/*
+ * event_send_speech.c
+ *
+ *  Created on: 19.05.2017
+ *      Author: michaelboeckling
+ */
+
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "freertos/FreeRTOS.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+#include "nghttp2/nghttp2.h"
+
+#include "nghttp2_client.h"
+#include "multipart_parser.h"
+#include "audio_player.h"
+#include "alexa.h"
+#include "audio_recorder.h"
+#include "common_buffer.h"
+#include "multipart_producer.h"
+#include "ui.h"
+#include "alexa_speech_recognizer.h"
+
+#include "include/alexa_events_js.h"
+
+#define TAG "event_send_speech"
+
+
+static speech_recognizer_state_t state;
+
+pcm_format_t buf_desc = {
+    .sample_rate = 16000,
+    .bit_depth = I2S_BITS_PER_SAMPLE_16BIT,
+    .num_channels = 1,
+    .buffer_format = PCM_LEFT_RIGHT
+};
+
+/* send data  */
+static bool yield = false;
+static uint16_t rounds = 0;
+static uint32_t bytes_out_total = 0;
+ssize_t send_speech_read_callback(nghttp2_session *session, int32_t stream_id,
+        uint8_t *buf, size_t buf_length, uint32_t *data_flags,
+        nghttp2_data_source *data_source, void *user_data)
+{
+    alexa_stream_t *alexa_stream = data_source->ptr;
+    ssize_t bytes_written = 0;
+
+    if(yield) {
+        yield = false;
+        // see https://github.com/nghttp2/nghttp2/pull/672/files#diff-20273fc5d9b0c6c133eb3d701444596d
+        return NGHTTP2_ERR_PAUSE;
+    }
+
+    buffer_t *buffer = buf_wrap(buf, buf_length);
+
+    switch (alexa_stream->next_action) {
+        case META_HEADERS:
+        case META_JSON:
+            ; // fix C grammar oddity
+
+            // write multipart headers
+            begin_part_meta_data(buffer);
+
+            // write json
+            char *json = create_evt_recognize(alexa_stream->msg_id++, alexa_stream->dialog_req_id++);
+            buf_write(buffer, json, strlen(json));
+            free(json);
+            bytes_written = buf_data_total(buffer);
+
+            alexa_stream->next_action = AUDIO_HEADERS;
+            break;
+
+        case AUDIO_HEADERS:
+            bytes_written = begin_part_audio_data(buffer);
+            alexa_stream->next_action = AUDIO_DATA;
+            // else won't print
+            printf("%.*s\n", bytes_written, buf);
+            break;
+
+        case AUDIO_DATA:
+            ;
+            /* Alexa wants:
+             * 16bit Linear PCM
+             * 16kHz sample rate
+             * Single channel
+             * Little endian byte order
+             */
+
+            // Amazon recommends 10ms of captured audio per chunk (320 bytes)
+            uint8_t *buf_ptr_read = buf;
+            uint8_t *buf_ptr_write = buf;
+
+            // read whole block of samples
+            int bytes_read = 0;
+            while(bytes_read == 0) {
+                bytes_read = i2s_read_bytes(I2S_NUM_1, (char*) buf, buf_length, 0);
+            }
+
+            //  convert 2x 32 bit stereo -> 1 x 16 bit mono
+            uint32_t samples_read = bytes_read / 2 / (I2S_BITS_PER_SAMPLE_32BIT / 8);
+
+            for(int i = 0; i < samples_read; i++) {
+                buf_ptr_write[0] = buf_ptr_read[2]; // mid
+                buf_ptr_write[1] = buf_ptr_read[3]; // high
+
+                buf_ptr_write += 1 * (I2S_BITS_PER_SAMPLE_16BIT / 8);
+                buf_ptr_read += 2 * (I2S_BITS_PER_SAMPLE_32BIT / 8);
+            }
+            bytes_written = samples_read * (I2S_BITS_PER_SAMPLE_16BIT / 8);
+
+            // local echo
+            //render_samples((char*) buf, bytes_written, &buf_desc);
+
+            rounds++;
+            if(rounds > 1) {
+                rounds = 0;
+                yield = true;
+            }
+
+            bytes_out_total += bytes_written;
+            printf("bytes_out: %d\n", bytes_written);
+
+            break;
+
+        case DONE:
+            audio_recorder_stop();
+            //renderer_stop();
+            ESP_LOGE(TAG, "DONE");
+            multipart_end(buffer);
+            *data_flags |= NGHTTP2_DATA_FLAG_EOF;
+            bytes_out_total = 0;
+
+            // reset state
+            alexa_stream->next_action = META_HEADERS;
+            break;
+    }
+
+    // printf("writing %d bytes to stream_id: %d, buf length: %d\n", bytes_written, stream_id, buf_length);
+
+    // printf("%d bytes out\n", bytes_written);
+    if(alexa_stream->next_action != AUDIO_DATA) {
+        printf("%.*s\n", bytes_written, buf);
+    }
+
+    //buf_destroy(buffer);
+    free(buffer);
+
+    return bytes_written;
+}
+
+
+void speech_recognizer_start_capture(alexa_session_t *alexa_session)
+{
+    state = SPEECH_RECOGNIZING;
+    //renderer_start();
+    audio_recorder_start();
+
+    alexa_stream_t *stream_events = get_stream_events(alexa_session);
+    stream_events->next_action = META_HEADERS;
+    stream_events->file_pos = 0;
+    stream_events->current_part = META_HEADERS;
+
+    alexa_send_event(alexa_session, send_speech_read_callback);
+}
+
+void speech_recognizer_stop_capture(alexa_session_t *alexa_session)
+{
+    //state = SPEECH_BUSY;
+    state = SPEECH_IDLE;
+    alexa_stream_t *stream_events = get_stream_events(alexa_session);
+    stream_events->next_action = DONE;
+    ui_queue_event(UI_NONE);
+}
+
+bool speech_recognizer_is_ready()
+{
+    return state == SPEECH_IDLE;
+}
+
+void speech_recognizer_set_state(speech_recognizer_state_t new_state)
+{
+    state = new_state;
+}
---
+++ main/app_main.c
@@ -0,0 +1,197 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+
+#include "http.h"
+
+
+#include "driver/i2s.h"
+
+#include "ui.h"
+#include "spiram_fifo.h"
+#include "audio_renderer.h"
+#include "audio_recorder.h"
+#include "web_radio.h"
+#include "playerconfig.h"
+#include "wifi.h"
+#include "app_main.h"
+#include "alexa_public.h"
+#include "mdns_task.h"
+#ifdef CONFIG_BT_SPEAKER_MODE
+#include "bt_speaker.h"
+#endif
+
+
+#define WIFI_LIST_NUM   10
+
+
+#define TAG "main"
+
+
+//Priorities of the reader and the decoder thread. bigger number = higher prio
+#define PRIO_READER configMAX_PRIORITIES -3
+#define PRIO_MQTT configMAX_PRIORITIES - 3
+#define PRIO_CONNECT configMAX_PRIORITIES -1
+
+
+
+static void alexa_task(void *pvParameters)
+{
+    alexa_init();
+    ESP_LOGI(TAG, "alexa_task stack: %d\n", uxTaskGetStackHighWaterMark(NULL));
+
+    // controls_init();
+
+    vTaskDelete(NULL);
+}
+
+
+static void init_hardware()
+{
+    nvs_flash_init();
+
+    // init UI
+    // ui_init(GPIO_NUM_32);
+
+    //Initialize the SPI RAM chip communications and see if it actually retains some bytes. If it
+    //doesn't, warn user.
+    if (!spiRamFifoInit()) {
+        printf("\n\nSPI RAM chip fail!\n");
+        while(1);
+    }
+
+    ESP_LOGI(TAG, "hardware initialized");
+}
+
+static void start_wifi()
+{
+    ESP_LOGI(TAG, "starting network");
+
+    /* FreeRTOS event group to signal when we are connected & ready to make a request */
+    EventGroupHandle_t wifi_event_group = xEventGroupCreate();
+
+    /* init wifi */
+    initialise_wifi(wifi_event_group);
+
+    /* start mDNS */
+    // xTaskCreatePinnedToCore(&mdns_task, "mdns_task", 2048, wifi_event_group, 5, NULL, 0);
+
+    /* Wait for the callback to set the CONNECTED_BIT in the event group. */
+    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
+                        false, true, portMAX_DELAY);
+}
+
+static renderer_config_t *create_renderer_config()
+{
+    renderer_config_t *renderer_config = calloc(1, sizeof(renderer_config_t));
+
+    renderer_config->bit_depth = I2S_BITS_PER_SAMPLE_16BIT;
+    renderer_config->i2s_num = I2S_NUM_0;
+    renderer_config->sample_rate = 44100;
+    renderer_config->sample_rate_modifier = 1.0;
+    renderer_config->output_mode = AUDIO_OUTPUT_MODE;
+
+    if(renderer_config->output_mode == I2S_MERUS) {
+        renderer_config->bit_depth = I2S_BITS_PER_SAMPLE_32BIT;
+    }
+
+    if(renderer_config->output_mode == DAC_BUILT_IN) {
+        renderer_config->bit_depth = I2S_BITS_PER_SAMPLE_16BIT;
+    }
+
+    return renderer_config;
+}
+
+static void start_web_radio()
+{
+    // init web radio
+    web_radio_t *radio_config = calloc(1, sizeof(web_radio_t));
+    radio_config->url = PLAY_URL;
+
+    // init player config
+    radio_config->player_config = calloc(1, sizeof(player_t));
+    radio_config->player_config->command = CMD_NONE;
+    radio_config->player_config->decoder_status = UNINITIALIZED;
+    radio_config->player_config->decoder_command = CMD_NONE;
+    radio_config->player_config->buffer_pref = BUF_PREF_SAFE;
+    radio_config->player_config->media_stream = calloc(1, sizeof(media_stream_t));
+
+    // init renderer
+    renderer_init(create_renderer_config());
+
+    // start radio
+    web_radio_init(radio_config);
+    web_radio_start(radio_config);
+}
+
+
+
+
+#include "common_buffer.h"
+#include "url_parser.h"
+#include "nghttp2/nghttp2.h"
+#include "nghttp2_client.h"
+#include "asio.h"
+#include "asio_http.h"
+#include "asio_http2.h"
+
+static void signal_strength()
+{
+    start_wifi();
+
+    wifi_ap_record_t ap_info;
+
+    while(1) {
+        esp_wifi_sta_get_ap_info(&ap_info);
+        printf("rssi: %" PRIi8 "\n", ap_info.rssi);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+}
+
+/**
+ * entry point
+ */
+void app_main()
+{
+    ESP_LOGI(TAG, "starting app_main()");
+    ESP_LOGW(TAG, "%d: - RAM left %d", __LINE__, esp_get_free_heap_size());
+
+    //signal_strength();
+
+    /* print MAC */
+    uint8_t sta_mac[6];
+    esp_efuse_mac_get_default(sta_mac);
+    ESP_LOGE(TAG, "MAC address: " MACSTR, MAC2STR(sta_mac));
+
+    init_hardware();
+
+#ifdef CONFIG_BT_SPEAKER_MODE
+    bt_speaker_start(create_renderer_config());
+#else
+
+    /*
+    ESP_LOGW(TAG, "%d: - RAM left %d", __LINE__, esp_get_free_heap_size());
+    //start_web_radio();
+    // can't mix cores when allocating interrupts
+    ESP_LOGW(TAG, "%d: - RAM left %d", __LINE__, esp_get_free_heap_size());
+    */
+    renderer_init(create_renderer_config());
+    audio_recorder_init();
+    xTaskCreatePinnedToCore(&alexa_task, "alexa_task", 16384, NULL, 1, NULL, 1);
+#endif
+
+    ESP_LOGW(TAG, "%d: - RAM left %d", __LINE__, esp_get_free_heap_size());
+    // ESP_LOGI(TAG, "app_main stack: %d\n", uxTaskGetStackHighWaterMark(NULL));
+    vTaskDelete(NULL);
+}
